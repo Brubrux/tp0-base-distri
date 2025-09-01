0efbf1242fa35fb071b2a5c0536603692335ea68
Brubrux <bortielli@fi.uba.ar>
2025-08-31 21:32:38 -0300

    primera iteracion del .sh

---
+++ validar-echo-server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FLAGS="-i --rm"
+NETWORK=tp0_testing_net
+MSG="Hola Server!"
+
+RES=$(docker run $FLAGS --network=$NETWORK busybox sh -c "echo $MSG | nc server 12345")
+
+if [ "$RES" == "$MSG" ]; then
+  echo "action: test_echo_server | result: success"
+else
+  echo "action: test_echo_server | result: fail"
+fi
